b812cabe61fc7d6b4b84cc24ff05bd2112ca64aa
chenzhizs <chenzhi@qq.com>
2021-04-14 12:33:46 +0900

    看完，差不多了解了

---
+++ SwiftUI Views in UIKit Apps Using UIHostingController/SwiftUI Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Views in UIKit Apps Using UIHostingController
+//
+//  Created by chenzhizs on 2021/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.yellow)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
